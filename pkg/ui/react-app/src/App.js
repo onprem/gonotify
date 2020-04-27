9e7df85befaebb8934b4e8d6992f82bdbb0bb1df
Prem Kumar <prmsrswt@gmail.com>
2020-04-28 03:52:49 +0530

    add proper logos

--- pkg/ui/react-app/src/App.js
+++ pkg/ui/react-app/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {ReactComponent as Logo} from './Assets/logo/logo.svg';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Go Notify
-        </a>
-      </header>
+    <div className={styles.App}>
+      <Logo className={styles.logo} />
     </div>
   );
 }
