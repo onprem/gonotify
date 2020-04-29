fdc02720f1724b24a013fc191756fe22a49cbf9d
Prem Kumar <prmsrswt@gmail.com>
2020-04-29 23:23:33 +0530

    ui: integrate register with backend

--- pkg/ui/react-app/src/Views/LogReg/Register/index.js
+++ pkg/ui/react-app/src/Views/LogReg/Register/index.js
@@ -1,21 +1,77 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import useSWRPost from 'Hooks/useSWRPost';
+
 import { Text } from 'Components/Form';
 import Button from 'Components/Button';
+
+import toast from 'Utils/toast';
 
 import { ReactComponent as ArrowIcon } from 'Assets/icons/arrow.svg';
 import styles from './register.module.css';
 
 const Register = () => {
+  const { handleSubmit, register, errors, getValues } = useForm();
+  const history = useHistory();
+
+  const [runRegister, { isValidating }] = useSWRPost('/api/v1/register', {
+    onSuccess: (data) => {
+      if (data.error) toast.error(data.error);
+      else {
+        toast.success(data.message);
+        history.push(`/verify/${getValues('phone')}`);
+      }
+    },
+    onError: toast.error,
+  });
+
+  useEffect(() => {
+    const fields = ['name', 'phone', 'password'];
+    fields.forEach((f) => {
+      if (errors[f]?.message) toast.error(errors[f].message);
+    });
+  });
+
   return (
     <>
       <h1 className={styles.heading}>Lets join !!</h1>
       <p className={styles.para}>Enter name, phone number and password to continue</p>
-      <form className={styles.form}>
-        <Text name="name" label="Full Name" placeholder="Irfan Khan" />
-        <Text name="phone" label="Your Phone" placeholder="+919912312345" />
-        <Text type="password" name="password" label="Password" placeholder="password" />
-        <Button className={styles.btn} type="submit">
+      <form className={styles.form} onSubmit={handleSubmit(runRegister)}>
+        <Text
+          name="name"
+          label="Full Name"
+          placeholder="Irfan Khan"
+          inpRef={register({
+            required: 'Name is required',
+          })}
+        />
+        <Text
+          name="phone"
+          label="Your Phone"
+          placeholder="+919912312345"
+          inpRef={register({
+            required: 'Phone is required',
+            pattern: {
+              value: /^\+?\d+$/,
+              message: 'Invalid phone number',
+            },
+          })}
+        />
+        <Text
+          type="password"
+          name="password"
+          label="Password"
+          placeholder="password"
+          inpRef={register({
+            required: 'Password is required',
+            minLength: {
+              value: 4,
+              message: 'Minimum password length is 4',
+            },
+          })}
+        />
+        <Button className={styles.btn} type="submit" disabled={isValidating}>
           Register <ArrowIcon />
         </Button>
       </form>
--- pkg/ui/react-app/src/Views/LogReg/Verify/index.js
+++ pkg/ui/react-app/src/Views/LogReg/Verify/index.js
@@ -1,5 +1,10 @@
-import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Link, useParams, useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
+import useSWRPost from 'Hooks/useSWRPost';
+import toast from 'Utils/toast';
+
 import { Text } from 'Components/Form';
 import Button from 'Components/Button';
 
@@ -8,15 +13,58 @@ import styles from '../logreg.module.css';
 
 const Verify = () => {
   const { phone } = useParams();
+  const history = useHistory();
+  const { handleSubmit, register, errors } = useForm();
+
+  const [runVerify, { isValidating }] = useSWRPost('/api/v1/verify', {
+    onSuccess: (data) => {
+      if (data.error) toast.error(data.error);
+      else {
+        toast.success(data.message);
+        history.push(`/login`);
+      }
+    },
+    onError: toast.error,
+  });
+
+  useEffect(() => {
+    const fields = ['phone', 'code'];
+    fields.forEach((f) => {
+      if (errors[f]?.message) toast.error(errors[f].message);
+    });
+  });
 
   return (
     <>
       <h1 className={styles.heading}>Lets join !!</h1>
       <p className={styles.para}>Verify your phone number to continue</p>
-      <form className={styles.form}>
-        <Text name="phone" label="Your Phone" value={phone} readOnly={true} />
-        <Text type="text" name="code" label="Verification Code" placeholder="123456" />
-        <Button className={styles.btn} type="submit">
+      <form className={styles.form} onSubmit={handleSubmit(runVerify)}>
+        <Text
+          name="phone"
+          label="Your Phone"
+          value={phone}
+          readOnly={true}
+          inpRef={register({
+            required: 'Phone number is required',
+            pattern: {
+              value: /^\+?\d+$/,
+              message: 'Invalid phone number',
+            },
+          })}
+        />
+        <Text
+          name="code"
+          label="Verification Code"
+          placeholder="123456"
+          inpRef={register({
+            required: 'Verification code is required',
+            pattern: {
+              value: /^\d{6}$/,
+              message: 'Invalid verification code',
+            },
+          })}
+        />
+        <Button className={styles.btn} type="submit" disabled={isValidating}>
           Verify <ArrowIcon />
         </Button>
       </form>
