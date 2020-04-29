1a7b16f0906e3317440a73e8f86abd6fca94657b
Prem Kumar <prmsrswt@gmail.com>
2020-04-30 02:32:11 +0530

    ui: dashboard: top nav

--- pkg/ui/react-app/src/Views/Dashboard/Nav/index.js
+++ pkg/ui/react-app/src/Views/Dashboard/Nav/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { useAuth } from 'Context/auth';
+
+import user from 'Assets/icons/acc.svg';
+import { ReactComponent as SettingIcon } from 'Assets/icons/settings.svg';
+
 import styles from './nav.module.css';
 
 const Nav = () => {
-  return <div className={styles.nav}></div>;
+  const { logMeOut } = useAuth();
+  return (
+    <div className={styles.nav}>
+      <Link to="/dashboard/account" className={styles.icoLink}>
+        <SettingIcon className={styles.ico} />
+      </Link>
+      <div className={styles.accDiv}>
+        <img src={user} alt="avatar" className={styles.avatar} />
+        <div className={styles.menu}>
+          <a className={styles.link} href="#logout" onClick={logMeOut}>
+            LogOut
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
